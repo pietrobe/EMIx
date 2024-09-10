f8dfcd024549c2ec57451732c757de7e6d2b2f52
Marius Causemann <mariusca@simula.no>
2024-09-10 13:53:10 +0200

    add configfile CLI

--- benchmarks/emi_spectral/EMI_spectral.py
+++ benchmarks/emi_spectral/EMI_spectral.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 from EMIx   import *
 from dolfin import *
+import argparse
 
 if __name__=='__main__':
-			
+	parser = argparse.ArgumentParser(description='Run EMI solver')
+	parser.add_argument('configfile', type=str, help='the config ile')
+	args = parser.parse_args()
+
 	# create EMI problem and ionic model
-	problem = EMI_problem('config.yml')	
+	problem = EMI_problem(args.configfile)	
 
 	# add HH ionic model
 	problem.add_ionic_model("Passive")
